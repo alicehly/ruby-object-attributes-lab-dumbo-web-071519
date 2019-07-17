aa43cd5e52959de061dc2a9e2e0abc4d0e71db1a
Alice Ly Ms <alicehly@gmail.com>
2019-07-17 01:51:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -10,4 +10,16 @@ class Person
   def name=(new_name)
     @name = new_name
   end
+  
+  def initialize(job)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = job
+  end 
 end
